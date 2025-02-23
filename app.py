6e5df2de3e9a7ce61be7c48445dd7cfee2fb534f
admin-at-595115466597 <willhwli@amazon.com>
2025-02-23 10:06:03 +0800

    modify /flask and show arch

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import platform
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def root():
     return 'Hello, World from EKS!'
+
+@app.route('/flask')
+def hello_world():
+    arch = platform.machine()
+    return 'Hello, World from EKS Flask App running on {arch}!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
